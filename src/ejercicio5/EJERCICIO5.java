e96db09ca80a7e9ead4d26ff86797872728d1dcb
Y3FF0 <yeffo32@gmail.com>
2023-03-04 21:45:37 -0500

    Ejercicio #5

---
+++ src/ejercicio5/EJERCICIO5.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio5;
+
+/**
+ *
+ * @author Jeffrey Santiago Navarro Espinosa
+ */
+public class EJERCICIO5 {
+
+   
+    public static void main(String[] args) {
+        float Suma,X,Y;
+        
+        Suma=0;
+        X=20;
+        Suma=Suma+X;
+        Y=40;
+        X=Y*Y+X;
+        Suma=Suma+X/Y;
+        System.out.println("EL VALOR DE LA SUMA ES: "+Suma);
+                
+                
+    
+    
+    
+    
+    }
+    
+}
